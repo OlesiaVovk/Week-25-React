50c0e76edc41102270da8a6afe5e15ae4374784d
OlesiaVovk <2kirkeugle@gmail.com>
2021-12-01 00:44:47 +0600

    Практическое задание и ответы на вопросы теста

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
+export const data = {
+  labels: ['CSS', 'HTML', 'React', 'JavaScript'],
+  datasets: [
+    {
+      label: 'Knowledge',
+      data: [10, 19, 3, 8],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+function App() {
+  return <Pie data={data} />;
+}
+
+export default App;
+
